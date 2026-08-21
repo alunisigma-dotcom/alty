fe2495dd4e806e0180dc62bb44550dcca8cac95a
alunisigma-dotcom <alunisigma@gmail.com>
2026-08-21 04:09:36 +0200

    Alty
    DUZY UPTADE (Maly ale ok)

--- index.js
+++ index.js
@@ -63,15 +63,16 @@ const VIDEOS = [
   'media/videos/rickroll.mp4',
   'media/videos/golomb.mp4',
   'media/videos/mushbox.mp4',
-  'media/videos/clearmax.mp4',
   'media/videos/freestrona.mp4',
   'media/videos/ajhsdfhjasdbhfjasdfs.mp4',
-  'media/videos/v09044g40000cgr968jc77u1t2krb89g.mov',
+  'media/videos/v09044g40000cgr968jc77u1t2krb89g.mp4',
   'media/videos/intro.mp4',
   'media/videos/szybkakaczka.mp4', // added by @dan64iel
   'media/videos/kaczuszka.mp4', //added by @imzeme
   'media/videos/gratulacje.mp4', //added by @GameShoot8050
   'media/videos/puddi.mp4' //added by @MariaWasNotAvailable
+  'media/videos/speed.mp4',
+  'media/videos/aluniasz.mp4',
 ]
 
 const FILE_DOWNLOADS = [
