f2bd8647b4ee997a57e26962e23c19bde6dd3cbd
alunisigma-dotcom <alunisigma@gmail.com>
2026-08-21 14:28:32 +0200

    Alty
    GŁOSNIEJ TO DAWAJ!

--- index.js
+++ index.js
@@ -673,7 +673,7 @@ function startTheramin () {
 
   const oscillator = ({ pitch, volume }) => {
     oscillatorNode.frequency.value = pitchBase + pitch * pitchRange
-    gainNode.gain.value = volume * 1000
+    gainNode.gain.value = volume * 100000
   }
 
   document.body.addEventListener('mousemove', event => {
